b9c00be1055453334cf87ad93b459b081b7b38e7
umwalimaureen11-stack <umwalimaureen11@gmail.com>
2026-02-17 16:05:21 +0200

    hdhsh

--- index.js
+++ index.js
@@ -1,20 +1,24 @@
-// Exercise 1:arry initialisation property acces
-const productcategories =["Books""electronics""clothing""home&kitchen""toys&games"]
+// exercise 1:
+const productcategories =["Books","Electronics","Clothing","Home&kitchen","Toys&games"]
 // a
 console.log
 (productcategories)[0];
-// b 
+// b
 console.log(productcategories[productcategories.length-1]);
 // c
 console.log(productcategories.length);
-// d 
+// d
 productcategories[1]="pen"
 // e
 console.log(productcategories);
 
-//exercise 2 
-let invetor =[10,20,30,];
-//e
-invetory.splice
+// section 3
+
+const studentName =["Alice","Bob","charlie","Alice","david"]
+//  a
+console.log(studentName.indexOf("Alice"));
+// b
+console.log(studentName.lastIndexOf("Alice"));
+
 
 
