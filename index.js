4e4c3c1c0a85b74cb77e16e72097f59c4b22e5ec
umwalimaureen11-stack <umwalimaureen11@gmail.com>
2026-02-17 15:14:34 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// Exercise 1:arry initialisation property acces
+const productcategories =["Books""electronics""clothing""home&kitchen""toys&games"]
+// a
+console.log
+(productcategories)[0];
+// b 
+console.log(productcategories[productcategories.length-1]);
+// c
+console.log(productcategories.length);
+// d 
+productcategories[1]="pen"
+// e
+console.log(productcategories);
+
+//exercise 2 
+let invetor =[10,20,30,];
+//e
+invetory.splice
+
+
